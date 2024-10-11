a3c04860092e5a310c00864701f95bb0c92ce211
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2024-10-11 09:25:30 +0800

    fix: :bug: 修复代码生成上级菜单回显

--- src/views/tools/codeGen/components/GenerateInfoForm.vue
+++ src/views/tools/codeGen/components/GenerateInfoForm.vue
@@ -9,11 +9,11 @@
             <!-- 包路径提示工具 -->
             <el-tooltip content="生成在哪个java包下，例如 com.ruoyi.system" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
-          <el-input v-model="table.packageName"/>
+          <el-input v-model="table.packageName" />
         </el-form-item>
       </el-col>
 
@@ -22,7 +22,7 @@
         <el-form-item prop="tplCategory">
           <template #label>生成模板</template>
           <el-select v-model="table.tplCategory" disabled>
-            <el-option label="单表（增删改查）" value="crud"/>
+            <el-option label="单表（增删改查）" value="crud" />
           </el-select>
         </el-form-item>
       </el-col>
@@ -35,11 +35,11 @@
             <!-- 模块名提示工具 -->
             <el-tooltip content="可理解为子系统名，例如 system" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
-          <el-input v-model="table.moduleName"/>
+          <el-input v-model="table.moduleName" />
         </el-form-item>
       </el-col>
 
@@ -51,11 +51,11 @@
             <!-- 业务名提示工具 -->
             <el-tooltip content="可理解为功能英文名，例如 user" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
-          <el-input v-model="table.businessName"/>
+          <el-input v-model="table.businessName" />
         </el-form-item>
       </el-col>
 
@@ -67,32 +67,33 @@
             <!-- 功能名提示工具 -->
             <el-tooltip content="用作类描述，例如 用户" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
-          <el-input v-model="table.functionName"/>
+          <el-input v-model="table.functionName" />
         </el-form-item>
       </el-col>
 
       <!-- 上级菜单 -->
       <el-col :span="12">
-        <el-form-item prop="parentMenuId">
+        <el-form-item>
           <template #label>
             上级菜单
             <!-- 菜单选择提示工具 -->
             <el-tooltip content="分配到指定菜单下，例如 系统管理" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
           <el-tree-select
             v-model="table.parentMenuId"
-            placeholder="选择上级菜单"
             :data="menuOptions"
-            filterable
+            placeholder="请选择系统菜单"
             check-strictly
+            filterable
+            clearable
             :render-after-expand="false"
           />
         </el-form-item>
@@ -106,7 +107,7 @@
             <!-- 生成代码方式提示工具 -->
             <el-tooltip content="默认为zip压缩包下载，也可以自定义生成路径" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
@@ -123,7 +124,7 @@
             <!-- 自定义路径提示工具 -->
             <el-tooltip content="填写磁盘绝对路径，若不填写，则生成到当前Web项目下" placement="top">
               <el-icon>
-                <question-filled/>
+                <question-filled />
               </el-icon>
             </el-tooltip>
           </template>
@@ -152,11 +153,11 @@
 
 <script lang="ts" setup>
 import MenuAPI from "@/api/system/menu";
-import {PropType} from "vue";
-import {CodeGenColumnsInfoVO, CodeGenTableInfoVO} from "@/api/tools/generator";
+import { PropType } from "vue";
+import { CodeGenColumnsInfoVO, CodeGenTableInfoVO } from "@/api/tools/generator";
 
 // 定义组件名称
-defineOptions({name: 'CodegenGenerateInfoForm'});
+defineOptions({ name: 'CodegenGenerateInfoForm' });
 
 // 接收来自父组件的 props，用于绑定表单数据
 const props = defineProps({
@@ -179,21 +180,24 @@ defineExpose({
 
 // 表单的校验规则
 const rules = ref({
-  tplCategory: [{required: true, message: "请选择生成模板", trigger: "blur"}],
-  packageName: [{required: true, message: "请输入生成包路径", trigger: "blur"}],
-  moduleName: [{required: true, message: "请输入生成模块名", trigger: "blur"}],
-  businessName: [{required: true, message: "请输入生成业务名", trigger: "blur"}],
-  functionName: [{required: true, message: "请输入生成功能名", trigger: "blur"}]
+  tplCategory: [{ required: true, message: "请选择生成模板", trigger: "blur" }],
+  packageName: [{ required: true, message: "请输入生成包路径", trigger: "blur" }],
+  moduleName: [{ required: true, message: "请输入生成模块名", trigger: "blur" }],
+  businessName: [{ required: true, message: "请输入生成业务名", trigger: "blur" }],
+  functionName: [{ required: true, message: "请输入生成功能名", trigger: "blur" }]
 });
 
 // 菜单选项列表，用于上级菜单的下拉选择
 const menuOptions = ref<OptionType[]>([]);
-
-// 组件挂载时加载菜单选项数据
-onMounted(async () => {
+function getMenuTreeSelect() {
   MenuAPI.getOptions(true).then((data) => {
     menuOptions.value = data;
   });
+}
+
+// 组件挂载时加载菜单选项数据
+onMounted(async () => {
+  await getMenuTreeSelect();
 });
 
 </script>
